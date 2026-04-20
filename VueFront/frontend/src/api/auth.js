6f18ba457dc95e2d5c58dbd8c58f64410284cab7
peachicedcoffee <142460548+peachicedcoffee@users.noreply.github.com>
2026-04-20 09:41:39 +0900

    Delete VueFront/frontend directory

--- VueFront/frontend/src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: 'http://localhost:5127/api',
-})
-
-export const login = (username, password) => api.post('/auth/login', { username, password })
